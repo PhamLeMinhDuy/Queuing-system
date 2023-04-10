a660e549c40e608c96a4307bd1560e8b34f95b7a
MinhDuy17072002 <duy.207ct68618@vanlanguni.vn>
2023-04-10 20:10:29 +0700

    report 8

--- src/pages/Auth/LoginPage/Login.tsx
+++ src/pages/Auth/LoginPage/Login.tsx
@@ -1,9 +1,47 @@
 import Forget from '../ForgetPage/Forget'
+import { useState } from 'react';
 import logo from '../logo/Logoalta.png'
 import image1 from './images/image1.png'
-import eye_off from './icon_eye/eye-off.png'
+import { EyeOff, Eye, AlertCircle } from 'react-feather';
+import firebase from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'; 
+import { auth } from '../../../firebase/config';
+import "firebase/auth";
 import './login.css'
+import { useNavigate } from 'react-router-dom';
 export const Login = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); 
+  const navigate = useNavigate();
+  const handleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, username, password) // Đăng nhập với username và password
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/inforpage");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+      });
+  }
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setUsername(e.target.value);
+  setError(false);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setPassword(e.target.value);
+  setError(false); // Reset error state when user types something
+  };
+
+const forgetPasswordClass = error ? 'forget-password error' : 'forget-password';
+  const handleShowPassword = () => { 
+    setShowPassword(!showPassword);
+  };
   return (
     <div className='login-page'>
       <div className="form login-form">
@@ -11,24 +49,33 @@ export const Login = () => {
           <img src={logo} alt="Đây là Logo của công ty Alta Media"></img>
         </div>
         <div className="form-input">
-          <div className="form-group">
+          <div className={`form-group ${error ? 'error' : ''}`}>
             <label htmlFor="username">Tên đăng nhập *</label> <br />
-            <input type="text" className="form-control" id="username" placeholder=" Username " />
+            <input type="email" className={`form-control ${error ? 'error' : ''}`} id="username" placeholder=" Username " onChange={handleUsernameChange} />
           </div>
-          <div className="form-group">
+          <div className={`form-group ${error ? 'error' : ''}`}>
             <label htmlFor="password">Mật khẩu *</label><br />
             <div className='from-control__pass'>
-              <input type="password" className="form-control" id="password" placeholder=" Password " />
-              <img className='eye-off' src={eye_off}></img>
+              <input  type={showPassword ? 'text' : 'password'} className={`form-control ${error ? 'error' : ''}`} id="password" placeholder=" Password " onChange={handlePasswordChange} />
+              {showPassword ? (
+                <Eye className="eye" size={20} onClick={handleShowPassword} />
+              ) : (
+                <EyeOff className="eye-off" size={20} onClick={handleShowPassword} />
+              )}
             </div>
             
           </div>
         </div>
         
         <div className="forget-password">
+        {error && (
+          <p className="error-message">
+            <span className="error-icon"><AlertCircle size={16} /></span>
+          </p>
+        )}
           <a href="#" className='forget-link'>Quên mật khẩu?</a>
         </div>
-        <input className='btn' type="submit" value="Đăng nhập"/>
+        <button className='primary-btn btn' type="submit" onClick={handleLogin}> Đăng nhập</button>
         
       </div>
       <div className='login-image'>
